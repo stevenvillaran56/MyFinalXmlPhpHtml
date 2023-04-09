f05856da161cf383d0438f9c24e5cf11e4056776
stevenvillaran <stevenvillaran56@gmail.com>
2023-04-10 02:26:07 +0800

    Done at Array in php

--- Beverages.php
+++ Beverages.php
@@ -3,55 +3,39 @@
     // root
     $Beverage = [
         [ 
-        "owner" => "Nestlé", 
-        "year" => "1938",
-        "country" => "Switzerland", 
-        "place" => "Swiss town of Orbe", 
-        "creator" => "Max Morgenthaler",
-        "products" => ["Nescafe creamy white","Nescafe Creamy latte","Nescafe Original","Nescafe classic"]            
+     "name" => "Nescafe",
+     "year" => "1938",
+     "country" => "Switzerland", 
+     "place" => "Swiss town of Orbe", 
+     "creator" => "Max Morgenthaler",
+     "products" => ["Nescafe creamy white","Nescafe Creamy latte","Nescafe Original","Nescafe classic"]            
+],
+[    "name" => "MinuteMaid",  
+     "year" => "1945",
+     "country" => "United States", 
+     "place" => "Sugar Land, Texas", 
+     "creator" => "John M. Fox",
+     "products" => ["AGUAS FRESCAS","ORANGE JUICES","LEMONADES AND FRUIT DRINKS","NVARIETY JUICES"]            
 ],
 
-    <MinuteMaid>
-        <owner> Minute Maid</owner>
-        <year> 1945 </year>
-        <country> United States </country>
-        <place> Sugar Land, Texas </place>
-        <creator > John M. Fox </creator >
-        <products>
-           <product id="1"> AGUAS FRESCAS</product>
-           <product id="2"> ORANGE JUICES</product>
-           <product id="3"> LEMONADES AND FRUIT DRINKS</product>
-           <product id="4"> NVARIETY JUICES</product>
-        </products>
-    </MinuteMaid>  
-   
-    <CocaCola>
-        <owner> Coca-Cola Company </owner>
-        <year> 1886 </year>
-        <country> United States </country>
-        <place> Georgia </place>
-        <creator> John Stith Pemberton</creator>
-        <products>
-           <product id="1"> Coca-Cola</product>
-           <product id="2"> Coca-Cola Zero Sugar</product>
-           <product id="3"> Coca-Cola light</product>
-           <product id="4"> Coca-Cola life</product>
-        </products>
-    </CocaCola>  
-   
-    <Vodka >
-        <owner> Vodka </owner>
-        <year> 1917  </year>
-        <country> Russia </country>
-        <place> St. Petersburg </place>
-        <creator > Isidore </creator >
-        <products>
-           <product id="1"> Smirnoff</product>
-           <product id="2"> Svedka</product>
-           <product id="3"> Soplica</product>
-           <product id="4"> Nemiroff</product>
-           </products>
-    </Vodka > 
+[    "name" => "CocaCola", 
+     "year" => "1886",
+     "country" => "United States", 
+     "place" => "Georgia", 
+     "creator" => "John Stith Pemberton",
+     "products" => ["Coca-Cola","Coca-Cola Zero Sugar","Coca-Cola light","Coca-Cola life"]            
+],
 
-</Beverage>
-    ]
+[    "name" => "Vodka", 
+     "year" => "1917",
+     "country" => "Russia", 
+     "place" => "St. Petersburg", 
+     "creator" => "Isidore",
+     "products" => ["Smirnoff","Svedka","Soplica","Nemiroff"]            
+],   
+
+               ];
+
+               $data = json_encode($Beverage);
+               echo $data;
+               ?>
