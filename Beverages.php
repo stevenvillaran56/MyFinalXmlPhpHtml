0c14db1e857f6d54385c27687af20e8ca0b0c553
stevenvillaran <stevenvillaran56@gmail.com>
2023-04-10 02:15:30 +0800

    Starting

---
+++ Beverages.php
@@ -0,0 +1,57 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    // root
+    $Beverage = [
+        [ 
+        "owner" => "Nestlé", 
+        "year" => "1938",
+        "country" => "Switzerland", 
+        "place" => "Swiss town of Orbe", 
+        "creator" => "Max Morgenthaler",
+        "products" => ["Nescafe creamy white","Nescafe Creamy latte","Nescafe Original","Nescafe classic"]            
+],
+
+    <MinuteMaid>
+        <owner> Minute Maid</owner>
+        <year> 1945 </year>
+        <country> United States </country>
+        <place> Sugar Land, Texas </place>
+        <creator > John M. Fox </creator >
+        <products>
+           <product id="1"> AGUAS FRESCAS</product>
+           <product id="2"> ORANGE JUICES</product>
+           <product id="3"> LEMONADES AND FRUIT DRINKS</product>
+           <product id="4"> NVARIETY JUICES</product>
+        </products>
+    </MinuteMaid>  
+   
+    <CocaCola>
+        <owner> Coca-Cola Company </owner>
+        <year> 1886 </year>
+        <country> United States </country>
+        <place> Georgia </place>
+        <creator> John Stith Pemberton</creator>
+        <products>
+           <product id="1"> Coca-Cola</product>
+           <product id="2"> Coca-Cola Zero Sugar</product>
+           <product id="3"> Coca-Cola light</product>
+           <product id="4"> Coca-Cola life</product>
+        </products>
+    </CocaCola>  
+   
+    <Vodka >
+        <owner> Vodka </owner>
+        <year> 1917  </year>
+        <country> Russia </country>
+        <place> St. Petersburg </place>
+        <creator > Isidore </creator >
+        <products>
+           <product id="1"> Smirnoff</product>
+           <product id="2"> Svedka</product>
+           <product id="3"> Soplica</product>
+           <product id="4"> Nemiroff</product>
+           </products>
+    </Vodka > 
+
+</Beverage>
+    ]
