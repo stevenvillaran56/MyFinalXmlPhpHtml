ae9e890e1ddad141285d7d097cb12932e32ecf36
stevenvillaran <stevenvillaran56@gmail.com>
2023-04-11 05:42:58 +0800

    Done Coding

--- beverage.php
+++ beverage.php
@@ -1,41 +1,51 @@
 <?php
     header('Access-Control-Allow-Origin: *');
     // root
-    $Beverage = [
-        [ 
+    $beverage = [
+     
+[    
+     
+     "path" => ["../img/nes/nes1.png","../img/nes/nes2.png","../img/nes/nes3.png","../img/nes/nes4.jpg"],
      "name" => "Nescafe",
      "year" => "1938",
      "country" => "Switzerland", 
-     "place" => "Swiss town of Orbe", 
      "creator" => "Max Morgenthaler",
      "products" => ["Nescafe creamy white","Nescafe Creamy latte","Nescafe Original","Nescafe classic"]            
 ],
-[    "name" => "MinuteMaid",  
+
+[   
+     
+    
+     "path" => ["../img/maid/maid1.png","../img/maid/maid2.png","../img/maid/maid3.png","../img/maid/maid4.png",],
+     "name" => "MinuteMaid",  
      "year" => "1945",
      "country" => "United States", 
-     "place" => "Sugar Land, Texas", 
      "creator" => "John M. Fox",
      "products" => ["AGUAS FRESCAS","ORANGE JUICES","LEMONADES AND FRUIT DRINKS","NVARIETY JUICES"]            
 ],
 
-[    "name" => "CocaCola", 
+[    
+    
+     "path" => ["../img/coke/coke1.png","../img/coke/coke2.png","../img/coke/coke3.png","../img/coke/coke5.png", ],  
+     "name" => "CocaCola", 
      "year" => "1886",
      "country" => "United States", 
-     "place" => "Georgia", 
      "creator" => "John Stith Pemberton",
      "products" => ["Coca-Cola","Coca-Cola Zero Sugar","Coca-Cola light","Coca-Cola life"]            
 ],
 
-[    "name" => "Vodka", 
+[   
+     
+     "path" => ["../img/vodka/ab1.png","../img/vodka/ab2.png","../img/vodka/ab3.png","../img/vodka/ab4.png"],
+      "name" => "Vodka", 
      "year" => "1917",
-     "country" => "Russia", 
-     "place" => "St. Petersburg", 
+     "country" => "Russia",     
      "creator" => "Isidore",
      "products" => ["Smirnoff","Svedka","Soplica","Nemiroff"]            
 ],   
-
                ];
 
-               $data = json_encode($Beverage);
+               $data = json_encode($beverage);
                echo $data;
-               ?>
+           
+           ?>
